102094ac9ea891d91b2a341cb949fa9e6424a52c
holly <547481045@qq.com>
2025-12-19 10:32:19 +0800

    优化黑白名单处理逻辑

--- server/proxy/proxy.go
+++ server/proxy/proxy.go
@@ -168,7 +168,7 @@ func (pxy *BaseProxy) GetWorkConnFromPool(src, dst net.Addr) (workConn net.Conn,
 			dstAddr, dstPortStr, _ = net.SplitHostPort(dst.String())
 			dstPort, _ = strconv.ParseUint(dstPortStr, 10, 16)
 		}
-		err := msg.WriteMsg(workConn, &msg.StartWorkConn{
+		err = msg.WriteMsg(workConn, &msg.StartWorkConn{
 			ProxyName: pxy.GetName(),
 			SrcAddr:   srcAddr,
 			SrcPort:   uint16(srcPort),
